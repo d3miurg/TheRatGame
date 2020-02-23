ad5319e3fd84da39df7e2608e6206861f3b0bac9
d3miurg <whu19551@gmail.com>
2020-02-23 23:41:24 +0300

    threading instead of asyncio

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import datetime
 import os
 import sys
-import asyncio
 import pdb
 import time
+
+from threading import Thread
 
 class date(object):
     minute = 0
@@ -14,18 +15,19 @@ class date(object):
         
     def __init__(self):
         dt = datetime.datetime.now()
+        
         self.minute = dt.minute
         self.hour = dt.hour
         self.day = dt.day
         self.month = dt.month
         self.year = dt.year
 
-    def retTime(self):
-        return '{}.{}.{}: {}:{}'.format(self.day,
-                                           self.month,
-                                           self.year,
-                                           self.hour,
-                                           self.minute)
+    def returnTime(self):
+        return '{}.{}.{}: {}:{}'.format(self.day, 
+                                        self.month, 
+                                        self.year,
+                                        self.hour,
+                                        self.minute)
 
     def changeTime(self):
         self.minute += 1
@@ -76,51 +78,51 @@ def showMainInf(message):
     if result == 1:
         os.system('cls')
 
-    print(dat.retTime())
+    print(dat.returnTime())
     print(message)
 
-async def mainLoop():
-
-    inp = input()
-
-    if inp == 'exit()':
-        raise SystemExit
-
-    else:
-        try:
-            #pdb.set_trace()
-            sys.stdout = open('temp.txt', 'w')
-            
-            exec(inp)
-            
-            sys.stdout.close()
-            sys.stdout = les
-
-            curOut = open('temp.txt', 'r')
-            showMainInf(message = curOut.read())
-            curOut.close()
-
-        except Exception as e:
-            sys.stdout.close()
-            sys.stdout = les
-            showMainInf(message = e)
-
-async def timer():
-    time.sleep(1)
-    dat.changeTime()                            
-
-async def start():
+def mainLoop():
     while True:
-        mainTask = asyncio.create_task(mainLoop())
-        timerTask = asyncio.create_task(timer())
-        
-        await mainTask
-        await timerTask
+        inp = input()
 
-nowTime = datetime.datetime.now()
+        if inp == 'exit()':
+            raise SystemExit
+
+        else:
+            try:
+                sys.stdout = open('temp.txt', 'w')
+            
+                exec(inp)
+            
+                sys.stdout.close()
+                sys.stdout = les
+
+                curOut = open('temp.txt', 'r')
+                showMainInf(message = curOut.read())
+                curOut.close()
+
+            except Exception as e:
+                sys.stdout.close()
+                sys.stdout = les
+                showMainInf(message = e)
+
+def timer():
+    while True:
+        time.sleep(1)
+        dat.changeTime()
+
+        if mainThread.is_alive() != True:
+            break
+
 les = sys.stdout
 dat = date()
 
 showMainInf(message = '')
 
-asyncio.run(start())
+mainThread = Thread(target = mainLoop)
+timerThread = Thread(target = timer)
+
+#pdb.set_trace()
+    
+mainThread.start()
+timerThread.start()
